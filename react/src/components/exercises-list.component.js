5088e0cfbe8ec5a9cca4101d2c93b5eeec855e73
kadawatha <bydhanushka@gmail.com>
2020-04-23 22:12:08 +0530

    5.show list of data

--- react/src/components/exercises-list.component.js
+++ react/src/components/exercises-list.component.js
@@ -1,18 +1,75 @@
 import React, {Component} from 'react';
+import {Link} from "react-router-dom";
+import axios from 'axios';
 
-class ExercisesListComponent extends Component {
+const Exercise = props => (
+    <tr>
+        <td>{props.exercise.username}</td>
+        <td>{props.exercise.description}</td>
+        <td>{props.exercise.duration}</td>
+        <td>{props.exercise.date.substring(0,10)}</td>
+        <td>
+            <Link className='btn btn-warning' to={"/edit/"+props.exercise.id}>edit</Link> | <a href="#" className="btn btn-danger" onClick={() => { props.deleteExercise(props.exercise.id) }}>delete</a>
+        </td>
+    </tr>
+);
+
+
+
+
+export default class ExercisesList extends Component {
+    constructor(props) {
+        super(props);
+
+        this.deleteExercise = this.deleteExercise.bind(this);
+
+        this.state = {exercises: []};
+    }
+
+    componentDidMount() {
+        axios.get('http://127.0.0.1:8000/api/exercises/')
+            .then(response => {
+                this.setState({ exercises: response.data })
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+    }
+
+    deleteExercise(id) {
+        axios.delete('http://127.0.0.1:8000/api/exercises/'+id)
+            .then(res => { console.log(res.data)});
+
+        this.setState({
+            exercises: this.state.exercises.filter(el => el.id !== id)
+        })
+    }
+
+    exerciseList() {
+        return this.state.exercises.map(currentexercise => {
+            return <Exercise exercise={currentexercise} deleteExercise={this.deleteExercise} key={currentexercise.id}/>;
+        })
+    }
+
     render() {
         return (
-            <div className='container'>
-               <p>
-                   <strong> exercises-list.component.js </strong> 👙
-                   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, commodi doloribus est eveniet fuga in iure magnam,
-                   minima minus obcaecati rem repellendus sed sint totam unde veniam vitae! Ratione,
-                   voluptatem!
-               </p>
+            <div>
+                <h3>Logged Exercises</h3>
+                <table className="table">
+                    <thead className="thead-light">
+                    <tr>
+                        <th>Username</th>
+                        <th>Description</th>
+                        <th>Duration</th>
+                        <th>Date</th>
+                        <th>Actions</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    { this.exerciseList() }
+                    </tbody>
+                </table>
             </div>
-        );
+        )
     }
 }
-
-export default ExercisesListComponent;
