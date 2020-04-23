bed35e11c2bcef897275d2b55d898333b126c621
kadawatha <bydhanushka@gmail.com>
2020-04-23 22:12:39 +0530

    5.edit exercise data finish

--- react/src/components/edit-exercise.component.js
+++ react/src/components/edit-exercise.component.js
@@ -1,18 +1,150 @@
 import React, {Component} from 'react';
+import axios from 'axios';
+import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
 
-class EditExerciseComponent extends Component {
+export default class EditExercise extends Component {
+    constructor(props) {
+        super(props);
+
+        this.onChangeUsername = this.onChangeUsername.bind(this);
+        this.onChangeDescription = this.onChangeDescription.bind(this);
+        this.onChangeDuration = this.onChangeDuration.bind(this);
+        this.onChangeDate = this.onChangeDate.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+        this.state = {
+            username: '',
+            description: '',
+            duration: 0,
+            date: new Date(),
+            users: []
+        }
+    }
+
+    componentDidMount() {
+        axios.get('http://127.0.0.1:8000/api/exercises/'+this.props.match.params.id)
+            .then(response => {
+                this.setState({
+                    username: response.data.username,
+                    description: response.data.description,
+                    duration: response.data.duration,
+                    date: new Date(response.data.date)
+                })
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+
+        axios.get('http://127.0.0.1:8000/api/users/')
+            .then(response => {
+                if (response.data.length > 0) {
+                    this.setState({
+                        users: response.data.map(user => user),
+                        username: response.data[0].id
+                    })
+                }
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+
+    }
+
+    onChangeUsername(e) {
+        this.setState({
+            username: e.target.value
+        })
+    }
+
+    onChangeDescription(e) {
+        this.setState({
+            description: e.target.value
+        })
+    }
+
+    onChangeDuration(e) {
+        this.setState({
+            duration: e.target.value
+        })
+    }
+
+    onChangeDate(date) {
+        this.setState({
+            date: date
+        })
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+
+        const exercise = {
+            username: this.state.username,
+            description: this.state.description,
+            duration: this.state.duration,
+            date: this.state.date
+        }
+
+        console.log(exercise);
+
+        axios.put('http://127.0.0.1:8000/api/exercises/' + this.props.match.params.id, exercise)
+            .then(res => console.log(res.data));
+
+        window.location = '/';
+    }
+
     render() {
         return (
-            <div className='container'>
-                 <p>
-                    <strong> edit-exercise.component.js </strong>  🎒
-                     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusantium,
-                     adipisci animi aspernatur at beatae deleniti eaque earum eos esse et
-                     fuga maiores nemo nesciunt odio reiciendis tempore totam veniam.
-                 </p>
+            <div>
+                <h3>Edit Exercise Log</h3>
+                <form onSubmit={this.onSubmit}>
+                    <div className="form-group">
+                        <label>Username: </label>
+                        <select ref="userInput"
+                                required
+                                className="form-control"
+                                value={this.state.username}
+                                onChange={this.onChangeUsername}>
+                            {
+                                this.state.users.map(function(user) {
+                                    return <option key={user.id} value={user.id}>{user.username}</option>;
+                                })
+                            }
+                        </select>
+                    </div>
+                    <div className="form-group">
+                        <label>Description: </label>
+                        <input  type="text"
+                                required
+                                className="form-control"
+                                value={this.state.description}
+                                onChange={this.onChangeDescription}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label>Duration (in minutes): </label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            value={this.state.duration}
+                            onChange={this.onChangeDuration}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label>Date: </label>
+                        <div>
+                            <DatePicker
+                                selected={this.state.date}
+                                onChange={this.onChangeDate}
+                            />
+                        </div>
+                    </div>
+
+                    <div className="form-group">
+                        <input type="submit" value="Edit Exercise Log" className="btn btn-primary" />
+                    </div>
+                </form>
             </div>
-        );
+        )
     }
 }
-
-export default EditExerciseComponent;
