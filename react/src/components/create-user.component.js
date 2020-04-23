acf33b2cffe50d7fdeed51e35772b39baa001260
kadawatha <bydhanushka@gmail.com>
2020-04-23 22:11:17 +0530

    5.create users finish

--- react/src/components/create-user.component.js
+++ react/src/components/create-user.component.js
@@ -1,18 +1,62 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 
-class CreateUserComponent extends Component {
+export default class CreateUser extends Component {
+    constructor(props) {
+        super(props);
+
+        this.onChangeUsername = this.onChangeUsername.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+        this.state = {
+            username: ''
+        }
+    };
+
+    onChangeUsername(e) {
+        this.setState({
+            username: e.target.value
+        })
+    };
+
+    onSubmit(e) {
+        e.preventDefault();
+
+        const user = {
+            username: this.state.username
+        };
+
+        console.log(user);
+
+        axios.post('http://127.0.0.1:8000/api/users/', user)
+            .then(res => console.log(res.data));
+
+        this.setState({
+            username: ''
+        })
+        window.location = '/';
+
+    };
+
     render() {
         return (
-            <div className='container'>
-              <p>
-                <strong>  create-user.component.js </strong>   🥈
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Adipisci assumenda deleniti dicta error est expedita harum hic iste laboriosam
-                  laudantium magnam pariatur, provident quae, quidem reiciendis sapiente sed, totam unde!
-              </p>
+            <div>
+                <h3>Create New User</h3>
+                <form onSubmit={this.onSubmit}>
+                    <div className="form-group">
+                        <label>Username: </label>
+                        <input  type="text"
+                                required
+                                className="form-control"
+                                value={this.state.username}
+                                onChange={this.onChangeUsername}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <input type="submit" value="Create User" className="btn btn-primary" />
+                    </div>
+                </form>
             </div>
-        );
+        )
     }
 }
-
-export default CreateUserComponent;
